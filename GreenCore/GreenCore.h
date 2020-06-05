81485309b707afc7ba9e73651de19956de6fe717
Alessandro De Stefano <alexdsgm98@gmail.com>
2020-06-05 10:23:38 +0200

    Initial Commit

---
+++ GreenCore/GreenCore.h
@@ -0,0 +1,19 @@
+//
+//  GreenCore.h
+//  GreenCore
+//
+//  Created by Alessandro De Stefano on 05/06/2020.
+//  Copyright © 2020 Alessandro De Stefano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreenCore.
+FOUNDATION_EXPORT double GreenCoreVersionNumber;
+
+//! Project version string for GreenCore.
+FOUNDATION_EXPORT const unsigned char GreenCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreenCore/PublicHeader.h>
+
+
